87a6ee5eb21a9d6769966931ee0cf0181ed93487
Luiz <luizfranzon05@hotmail.com>
2022-11-06 14:58:27 -0300

    Fixed typescript error

--- App/src/pages/api/video.ts
+++ App/src/pages/api/video.ts
@@ -3,11 +3,14 @@ import twitterGetUrl from "twitter-url-direct";
 
 async function getVideoDownload(url: any) {
   let response = await twitterGetUrl(url);
-  return response
+  return response;
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { videoUrl } = req.query;
-  var data = await getVideoDownload(videoUrl)
+  var data = await getVideoDownload(videoUrl);
   res.status(200).json(data);
 }
